b12176f5283f78c1c7fefbf4ce01c6bc20554a7d
zhang874010534 <zhangce233@qq.com>
2019-11-20 14:35:14 +0800

    newlist

---
+++ src/components/newlist/newlist.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<h3>新闻列表 </h3>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style scoped="scoped" lang="scss">
+</style>
--- src/router.js
+++ src/router.js
@@ -4,10 +4,12 @@ import cart from './components/tabbar/cart.vue'
 import home from './components/tabbar/home.vue'
 import member from './components/tabbar/member.vue'
 import search from './components/tabbar/search.vue'
+import newlist from './components/newlist/newlist.vue'
 var router = new VueRouter({
 	routes: [
 		{path:'/',redirect:'/home'},
 		{path:'/home',component:home},
+		{path:'/home/newlist',component:newlist},
 		{path:'/member',component:member},
 		{path:'/cart',component:cart},
 		{path:'/search',component:search},
